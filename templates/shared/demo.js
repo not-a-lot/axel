d4a96b18b2bc99878012001d83a0fc7b7b1b2744
Roger Vion <roger.vion@epfl.ch>
2017-04-02 19:02:50 +0200

    Simplified templates/shared/demo.js and fixed the bug related to the ampersands in the options. dumpXML still needs some work.

--- templates/shared/demo.js
+++ templates/shared/demo.js
@@ -3,53 +3,45 @@ $(
   // Generates and insert the source code before the fragment
   // NOTE: MUST be called before transforming the template !
   function sourceDemo () {
+
     // FIXME: dumpXML works only with terminal nodes or with nodes containing only terminal nodes
-    function dumpXML (el) {
-      var tmp, prefix = '',
-          n = el.nodeName.toLowerCase(), 
-          accu = [];
-      if ((n === 'xt:use') || (n === 'xt:attribute')) {
-        if (n.charAt(2) === ':') {
-          n = n.substr(3);
-        }
-        prefix = 'xt:';
+    function dumpXML(el) {
+
+      function escape(s) {
+        return s.replace(/&/g, '&amp;');
       }
-      $.each(el.attributes, 
-            function(index, attr) {
-              accu.push(attr.nodeName + "='" + attr.nodeValue + "'"); 
-            });
+
+      let tmp, accu = [];
+      const nodeName = el.nodeName.toLowerCase();
+      $.each(el.attributes, function(index, attr) {
+        accu.push(attr.nodeName + "='" + escape(attr.nodeValue) + "'");
+      });
       if (el.children.length > 0) { // non text content 
         tmp = [];
-        $.each(el.children, 
-              function(index, el) {
-                tmp.push(dumpXML(el)); 
-              });           
-        return '&lt;' + prefix + n + ' ' + accu.join(' ') + '>' + tmp.join() + '&lt;/' + prefix + n + '>';
+        $.each(el.children, function(index, el) {
+          tmp.push(dumpXML(el));
+        });
+        return '&lt;' + nodeName + ' ' + accu.join(' ') + '>' + tmp.join() + '&lt;/' + nodeName + '>';
       } else if (el.textContent) {
-        return '&lt;' + prefix + n + ' ' + accu.join(' ') + '>' + el.textContent.replace('&', '&amp;', 'g').replace('<', '&amp;lt;', 'g') + '&lt;/' + prefix + n + '>';
+        return '&lt;' + nodeName + ' ' + accu.join(' ') + '>' + escape(el.textContent).replace(/</g, '&lt;') + '&lt;/' + nodeName + '>';
       } else {
-        return '&lt;' + prefix + n + ' ' + accu.join(' ') + '/>';
+        return '&lt;' + nodeName + ' ' + accu.join(' ') + '/>';
       }
     }
-    $('div.extract').each( 
-      function(index, el) { 
-        var buffer = [],
-            items = this.getElementsByTagNameNS("*", "*");
-        $.each(items,
-          function (index, el) {
-            var accu;
-            var n = el.nodeName.toLowerCase();
-            if ((n === 'xt:use') || (n === 'xt:attribute')) {
-              if (n.charAt(2) == ':') {
-                n = n.substr(3);
-              }
-              buffer.push(dumpXML(el));
-            }
-          }
-        );
-        var src = "<div class='source'><pre>" + buffer.join('<br/>') + "</pre></div>";
-        $(this).before(src); 
+
+    document.querySelectorAll("div.extract").forEach(function(div) {
+      const useElems = div.getElementsByTagNameNS("http://ns.inria.org/xtiger", "use");
+      const attrElems = div.getElementsByTagNameNS("http://ns.inria.org/xtiger", "attribute");
+      const xtElems = Array.from(useElems).concat(Array.from(attrElems));
+      const buffer = xtElems.map(function(elem) {
+        return dumpXML(elem);
+      });
+      const src = "<div class='source'><pre>" + buffer.join('<br/>') + "</pre></div>";
+      try {
+        div.insertAdjacentHTML('beforebegin', src);
+      } catch (e) {
+        console.error(e);
       }
-    ); 
+    });
   }
 );
